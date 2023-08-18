132467dc48a9c85798755f0d68a7c5b87455f520
Wren <OtherwiseJunk@gmail.com>
2023-08-17 23:26:59 -0400

    add audio add simple memey build settings

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -6,25 +6,32 @@ using UnityEngine.UI;
 public class Game : MonoBehaviour
 {
     public Text potsOfSlopText;
+
     public float potsOfSlop;
     public float slopPerClick;
 
+    private AudioSource _audioSource;
     // Start is called before the first frame update
     void Start()
     {
         potsOfSlop = 0;
         slopPerClick = 1;
+        _audioSource = GetComponent<AudioSource>();
 
+        if( _audioSource != null)
+        {
+            Debug.LogError("Failed to find AudioSource.");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void Click()
     {
+        _audioSource.Play();
         potsOfSlop += slopPerClick;
         UpdateScore();
     }
