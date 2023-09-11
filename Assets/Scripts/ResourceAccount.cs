8c7cca293b23ae1413b0e8749565ec914530b6bd
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-09-11 13:50:06 -0700

    basic resource structure

---
+++ Assets/Scripts/ResourceAccount.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceAccount : MonoBehaviour
+{
+    [SerializeField]
+    private Dictionary<ResourceDef, Resource> _dict = new(); 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
